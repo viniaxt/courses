c13f11d4428d53a83ebea0016f0dd81da2a4ef0c
viniaxt <viniciusaxt@gmail.com>
2019-08-19 19:38:14 -0300

    updating app with shop page, collection preview component and collection item component.

--- react-developer/crwn-clothing/src/App.js
+++ react-developer/crwn-clothing/src/App.js
@@ -1,29 +1,9 @@
 import React from "react"
-import { Route, Switch, Link } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import "./App.css"
 import HomePage from "./pages/homepage/homepage.component"
-
-
-const TopicsList = (props) => {
-  return (
-    <div>
-      <h1>Topics List</h1>
-      <Link to={`${props.match.url}/1`}> 1 </Link>
-      <Link to={`${props.match.url}/2`}> 2 </Link>
-      <Link to={`${props.match.url}/3`}> 3 </Link>
-    </div>
-  )
-}
-
-const TopicDetail = (props) => {
-  return (
-    <div>
-      <button onClick={() => props.history.push('/topics')}>Topics</button>
-      <h1>Topic detail => {props.match.params.topicId}</h1>
-    </div>
-  )  
-} 
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
@@ -31,8 +11,7 @@ function App() {
       {/* Switch renders route(s) that matches with URL*/}
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/topics' component={TopicsList} />
-        <Route path='/topics/:topicId' component={TopicDetail}/>
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   )
