f535b37898346ecd07312be0f3d0cd3c49b46305
viniaxt <viniciusaxt@gmail.com>
2019-08-19 17:57:34 -0300

    Update menu-item to navigate withRouter HOC

--- react-developer/crwn-clothing/src/App.js
+++ react-developer/crwn-clothing/src/App.js
@@ -1,11 +1,39 @@
 import React from "react"
+import { Route, Switch, Link } from 'react-router-dom'
+
 import "./App.css"
 import HomePage from "./pages/homepage/homepage.component"
+
+
+const TopicsList = (props) => {
+  return (
+    <div>
+      <h1>Topics List</h1>
+      <Link to={`${props.match.url}/1`}> 1 </Link>
+      <Link to={`${props.match.url}/2`}> 2 </Link>
+      <Link to={`${props.match.url}/3`}> 3 </Link>
+    </div>
+  )
+}
+
+const TopicDetail = (props) => {
+  return (
+    <div>
+      <button onClick={() => props.history.push('/topics')}>Topics</button>
+      <h1>Topic detail => {props.match.params.topicId}</h1>
+    </div>
+  )  
+} 
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* Switch renders route(s) that matches with URL*/}
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/topics' component={TopicsList} />
+        <Route path='/topics/:topicId' component={TopicDetail}/>
+      </Switch>
     </div>
   )
 }
--- react-developer/crwn-clothing/src/components/directory/directory.component.jsx
+++ react-developer/crwn-clothing/src/components/directory/directory.component.jsx
@@ -49,9 +49,12 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => {
+        {this.state.sections.map(({ id, ...otherSectionProps }) => {
           return (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+            <MenuItem 
+              key={id}
+              { ...otherSectionProps }
+            />
           )
         })}
       </div>
